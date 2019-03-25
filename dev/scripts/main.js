879207157e2bf5ed647c8cba53f11f7468b50545
caropi <pisanocaroline@gmail.com>
2019-03-25 16:56:15 -0400

    Started carousel JS and updated MD a little more

---
+++ dev/scripts/main.js
@@ -0,0 +1,21 @@
+const blogPosts = [
+  // USE BELOW CODE AS TEMPLATE FOR FUTURE BLOG POSTS 
+  //   please take care in separating each object with a comma
+//   {
+//     imgURL: "",
+//     linkURL: "",
+//     postDesc: ""
+//   }
+  {
+    imgURL: "test",
+    linkURL: "test",
+    postDesc: "test"
+  }
+];
+
+// We initialize a function that will target the existing HTML and input a random blog post in the carousel
+function getRandomPost() {
+    // We first get a random index in the array
+    const randomIndex = blogPosts[Math.floor(Math.random() * blogPosts.length)];
+    console.log(randomIndex)
+}
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,1 +1,22 @@
 "use strict";
+
+var blogPosts = [
+// USE BELOW CODE AS TEMPLATE FOR FUTURE BLOG POSTS 
+//   please take care in separating each object with a comma
+//   {
+//     imgURL: "",
+//     linkURL: "",
+//     postDesc: ""
+//   }
+{
+  imgURL: "test",
+  linkURL: "test",
+  postDesc: "test"
+}];
+
+// We initialize a function that will target the existing HTML and input a random blog post in the carousel
+function getRandomPost() {
+  // We first get a random index in the array
+  var randomIndex = blogPosts[Math.floor(Math.random() * blogPosts.length)];
+  console.log(randomIndex);
+}
