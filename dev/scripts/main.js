bedb6ec54ab795a887efe6d2e94ddc96653455fc
caropi <pisanocaroline@gmail.com>
2019-03-26 11:09:48 -0400

    Testing carousel

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -7,10 +7,20 @@ const blogPosts = [
 //     postDesc: ""
 //   }
   {
-    imgURL: "test",
-    linkURL: "test",
-    postDesc: "test"
-  }
+        imgURL: "https://static.indigoimages.ca/2019/test1carousel.png",
+    linkURL: "indigo.ca",
+    postDesc: "first blog post"
+  },
+  {
+      imgURL: "https://static.indigoimages.ca/2019/test2carousel.png",
+    linkURL: "indigo.ca",
+    postDesc: "second blog post"
+},
+    {
+        imgURL: "https://static.indigoimages.ca/2019/test3carousel.png",
+    linkURL: "indigo.ca",
+    postDesc: "third blog post"
+}
 ];
 
 // We initialize a function that will target the existing HTML and input a random blog post in the carousel
@@ -18,4 +28,7 @@ function getRandomPost() {
     // We first get a random index in the array
     const randomIndex = blogPosts[Math.floor(Math.random() * blogPosts.length)];
     console.log(randomIndex)
+    document.getElementById('someid')
 }
+
+getRandomPost();
